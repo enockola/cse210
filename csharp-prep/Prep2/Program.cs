fd8a1319502780c7006cebe3f4c48f7cdf65e1b8
Enoch Olayemi <113262589+EnokOla@users.noreply.github.com>
2024-04-25 22:32:34 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercent = Console.ReadLine();
+        int grade = int.Parse(gradePercent);
+        int lastDigit = (grade % 10);
+
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        // if statements to filter out A+, F+, and F-
+        if (lastDigit >= 7 && grade < 93 && grade >59)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3 && grade > 59)
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade letter is '{letter}{sign}'");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+
+        else
+        {
+            Console.WriteLine("You have failed this class! Better luck next time");
+        }
     }
 }
