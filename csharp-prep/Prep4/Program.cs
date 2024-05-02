eed1fdd470b8daf58786e58947b7eb5cd2f9f7e0
Enoch Olayemi <113262589+enockola@users.noreply.github.com>
2024-05-02 13:19:35 -0600

    prep4 commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int number = -9999;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
+        int min = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num < min & num > 0)
+            {
+                min = num;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {min}");
+
+        numbers.Sort();
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
