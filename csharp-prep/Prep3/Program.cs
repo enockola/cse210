fd8062721cac7f054a5b7df3329846aaf5ed41a6
Enoch Olayemi <113262589+enockola@users.noreply.github.com>
2024-05-02 11:53:15 -0600

    prep3 commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Do you want to play Number Guesser? ");
+        string response = Console.ReadLine();
+
+        while (response.ToLower() != "no")
+        {
+            if (response.ToLower() == "yes")
+            {
+                Random randomGenerator = new Random();
+                int number = randomGenerator.Next(1, 100);
+
+                int guess = 0;
+
+                int count = 0;
+
+                Console.WriteLine($"What is the magic number? {number}");
+
+                while (guess != number)
+                {
+                    Console.Write("What is your guess? ");
+                    guess = int.Parse(Console.ReadLine());
+                    count++;
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"It took you {count} guesses");
+                    }
+                }
+                Console.Write("Do you want to play again? ");
+                response = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Response!");
+                Console.Write("Do you want to play again? ");
+                response = Console.ReadLine();
+            }
+
+        }
+        Console.WriteLine("Thanks for playing Number Guesser!");
     }
 }
